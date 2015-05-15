0da3b53207f9e648d64f82b083f0f4c1c5a031fe
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2015-05-15 23:27:12 +0200

    Dev passwords must be longer than or equal to 6 chars.
    Refs #6

--- setup_organization.sh
+++ setup_organization.sh
@@ -7,9 +7,9 @@ echo "insert into feature_flags (id,name, restricted) VALUES (2, 'georef_disable
 ORGANIZATION_NAME="example"
 USERNAME="admin4example"
 EMAIL="admin@example.com"
-PASSWORD="pass"
+PASSWORD="pass1234"
 
 rake cartodb:db:create_user EMAIL="${EMAIL}" PASSWORD="${PASSWORD}" SUBDOMAIN="${USERNAME}"
 rake cartodb:db:set_unlimited_table_quota["${USERNAME}"]
-rake cartodb:db:create_new_organization_with_owner ORGANIZATION_NAME="${ORGANIZATION_NAME}" USERNAME="${USERNAME}" ORGANIZATION_SEATS=100 ORGANIZATION_QUOTA=102400
+rake cartodb:db:create_new_organization_with_owner ORGANIZATION_NAME="${ORGANIZATION_NAME}" USERNAME="${USERNAME}" ORGANIZATION_SEATS=100 ORGANIZATION_QUOTA=102400 ORGANIZATION_DISPLAY_NAME="${ORGANIZATION_NAME}"
 rake cartodb:db:set_organization_quota[$ORGANIZATION_NAME,100]
