7704e1cfa7baa61bb7942f3f3a0e73a4f3ae91c8
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2017-04-03 22:44:25 +0200

    Increase organization quota to 5Gb

--- setup_organization.sh
+++ setup_organization.sh
@@ -9,4 +9,4 @@ PASSWORD="pass1234"
 rake cartodb:db:create_user EMAIL="${EMAIL}" PASSWORD="${PASSWORD}" SUBDOMAIN="${USERNAME}"
 rake cartodb:db:set_unlimited_table_quota["${USERNAME}"]
 rake cartodb:db:create_new_organization_with_owner ORGANIZATION_NAME="${ORGANIZATION_NAME}" USERNAME="${USERNAME}" ORGANIZATION_SEATS=100 ORGANIZATION_QUOTA=102400 ORGANIZATION_DISPLAY_NAME="${ORGANIZATION_NAME}"
-rake cartodb:db:set_organization_quota[$ORGANIZATION_NAME,100]
+rake cartodb:db:set_organization_quota[$ORGANIZATION_NAME,5000]
