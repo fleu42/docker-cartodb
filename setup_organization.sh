af47cc8cce4c5ba2c32305e46b959747f68b7359
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2019-11-07 17:10:40 +0100

    Increase import limits
    Fixes #80

--- setup_organization.sh
+++ setup_organization.sh
@@ -5,9 +5,11 @@ PASSWORD="pass1234"
 
 bundle exec rake cartodb:db:create_user EMAIL="${EMAIL}" PASSWORD="${PASSWORD}" SUBDOMAIN="${USERNAME}"
 bundle exec rake cartodb:db:set_unlimited_table_quota["${USERNAME}"]
+bundle exec rake cartodb:db:set_user_quota["${USERNAME}",102400]
 bundle exec rake cartodb:db:create_new_organization_with_owner ORGANIZATION_NAME="${ORGANIZATION_NAME}" USERNAME="${USERNAME}" ORGANIZATION_SEATS=100 ORGANIZATION_QUOTA=102400 ORGANIZATION_DISPLAY_NAME="${ORGANIZATION_NAME}"
 bundle exec rake cartodb:db:set_organization_quota[$ORGANIZATION_NAME,5000]
 bundle exec rake cartodb:db:configure_geocoder_extension_for_organizations[$ORGANIZATION_NAME]
+bundle exec rake cartodb:set_custom_limits_for_user["${USERNAME}",10240000000,100000000,1]
 
 # Enable sync tables
 echo "UPDATE users SET sync_tables_enabled=true WHERE username='${USERNAME}'" | psql -U postgres -t carto_db_development
